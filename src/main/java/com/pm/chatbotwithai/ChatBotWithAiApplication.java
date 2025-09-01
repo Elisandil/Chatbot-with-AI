4fd86dc3734005b938365eac4a814c2a30710c76
aog-developer <elisandil88@gmail.com>
2025-09-01 07:39:30 +0200

    Initial commit

---
+++ src/main/java/com/pm/chatbotwithai/ChatBotWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.pm.chatbotwithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotWithAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotWithAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/chatbotwithai/ChatBotWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.chatbotwithai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotWithAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
